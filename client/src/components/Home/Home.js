c52f9b45f18aef344b3ba5750d7d1d8d5689bc9e
AlinaShahmur <ashaturnaya@gmail.com>
2021-10-03 15:33:38 +0300

    title was changed

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
         <section className = "section">
             <div className = "main"> 
                 <h1 className = 'title-h2'>Alina Shahmurov</h1>
-                <h2 className = 'title-h2'>Frontend developer</h2>
+                <h2 className = 'title-h2'>Full Stack Web developer</h2>
                 <Link className = 'home-link' to = "/about">Start</Link>
             </div>
         </section>
