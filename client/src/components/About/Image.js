3bff5e3771923c05a0dc2fe3c540399c8966a294
AlinaShahmur <ashaturnaya@gmail.com>
2023-07-31 16:52:37 +0300

    Changing the image url in 'about' section

--- client/src/components/About/Image.js
+++ client/src/components/About/Image.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Image() {
     return (
-        <img alt = "photo_Alina_Shahmurov" style = {{width: '300px', height: '100%', textAlign: "center"}} src = 'https://res.cloudinary.com/ds609wv4s/image/upload/v1627043662/RAS_2900_j61zd6.jpg'></img>
+        <img alt = "photo_Alina_Shahmurov" style = {{width: '300px', height: '100%', textAlign: "center"}} src = 'https://res.cloudinary.com/ds609wv4s/image/upload/v1690805645/meee_hoh7zz.jpg'></img>
     )
 }
